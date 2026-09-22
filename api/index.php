b80bbd457ea8d0eba3d830fbf7d2d637be3325d4
Ahmad Duwi Setianto <setiantoahmad02@gmail.com>
2026-09-22 15:13:35 +0700

    debug: tampilkan error php

--- api/index.php
+++ api/index.php
@@ -1,4 +1,8 @@
 <?php
 
+// Tampilkan semua error fatal PHP ke layar browser
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
 // Vercel Serverless Function entry point for Laravel
 require __DIR__ . '/../public/index.php';
